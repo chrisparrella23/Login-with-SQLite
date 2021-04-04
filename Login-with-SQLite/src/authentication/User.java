ca52c5f9472f0ca6f483fa712bc11b27731879f1
Chris Parrella <61067891+chrisparrella23@users.noreply.github.com>
2021-04-04 13:02:53 -0400

    Added stuff to User class
    -Created constructor for User object
    -Created LoginTest class

--- Login-with-SQLite/src/authentication/User.java
+++ Login-with-SQLite/src/authentication/User.java
@@ -1,5 +1,20 @@
 package authentication;
 
 public class User {
-
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private String email;
+	private String image;
+	
+	public User(String username, String password, String firstName, String lastName, String phone, String email) {
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+		this.email = email;
+	}
 }
--- Login-with-SQLite/src/test/LoginTest.java
+++ Login-with-SQLite/src/test/LoginTest.java
@@ -1,5 +1,15 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class LoginTest {
+
+	@Test
+	public void testLogin() {
+		
+		fail("Not yet implemented");
+	}
 
 }
