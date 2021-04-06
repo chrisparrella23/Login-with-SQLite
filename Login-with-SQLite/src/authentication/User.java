5d8fd46f988144e14eeb7fc37f9c2d10e0edf968
Chris Parrella <61067891+chrisparrella23@users.noreply.github.com>
2021-04-05 20:51:01 -0400

    Created ConnectionUtil class
    -Made getters/setters/toString for User

--- Login-with-SQLite/src/authentication/User.java
+++ Login-with-SQLite/src/authentication/User.java
@@ -17,4 +17,68 @@ public class User {
 		this.phone = phone;
 		this.email = email;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", phone=" + phone + ", email=" + email + ", image=" + image + "]";
+	}
+	
+	
 }
