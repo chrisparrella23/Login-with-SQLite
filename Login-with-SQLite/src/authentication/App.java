4efa90990b779777c712869715afa0f652e2d4ef
Chris Parrella <61067891+chrisparrella23@users.noreply.github.com>
2021-04-08 19:21:56 -0400

    Wrote main App file
    -Wrote initial App file

---
+++ Login-with-SQLite/src/authentication/App.java
@@ -0,0 +1,40 @@
+package authentication;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	private static Stage stg;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		stg = primaryStage;
+		primaryStage.setResizable(false);
+		
+		Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+		primaryStage.setTitle("Log In");
+		primaryStage.setScene(new Scene(root, 750, 700));
+		primaryStage.show();
+	}
+	
+	public void changeScene(String fxml) {
+		Parent pane;
+		try {
+			pane = FXMLLoader.load(getClass().getResource(fxml));
+			stg.getScene().setRoot(pane);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
