baa7ea55f1f579ae1c64b44fa30392f34f67dcd4
Chris Parrella <61067891+chrisparrella23@users.noreply.github.com>
2021-04-25 22:19:19 -0400

    Created to-do list for password test

--- Login-with-SQLite/src/test/PasswordChangeTest.java
+++ Login-with-SQLite/src/test/PasswordChangeTest.java
@@ -7,7 +7,21 @@ import org.junit.Test;
 public class PasswordChangeTest {
 
 	@Test
-	public void passwordChangeTest() {
+	public void passwordChangeTestSuccess() {
+		// password change method could return a boolean
+		// method needs to:
+		// take string for new password
+		// open connection (EZ PZ)
+		// use UPDATE method to change password with username as 
+		// WHERE condition (EZ PZ)
+		// close connection
+		assertTrue(changePassword(String username, String newPassword));
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void passwordChangeTestFail() {
+		
 		fail("Not yet implemented");
 	}
 
